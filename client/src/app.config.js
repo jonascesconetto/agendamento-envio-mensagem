c60d18eb74d8fe024800e404809924e31a3927b0
Jonas Cesconetto <jonascesconetto@gmail.com>
2020-11-14 15:58:06 -0300

    Conection with server and other configs

---
+++ client/src/app.config.js
@@ -0,0 +1,7 @@
+// const { config } = require("react-transition-group");
+const appConfig = {};
+
+appConfig.URL_SERVER = "http://localhost:4050/app/api/";
+appConfig.version = "1.0.0";
+
+export { appConfig };
