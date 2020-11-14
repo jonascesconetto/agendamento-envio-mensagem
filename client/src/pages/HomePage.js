6c99372de054845b6e84217e1c52525b69d2af6a
Jonas Cesconetto <jonascesconetto@gmail.com>
2020-11-14 17:11:02 -0300

    Update Home page

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -10,7 +10,9 @@ export class HomePage extends Component {
                 <div className="p-col-12">
                     <div className="card">
                         <h1>Home</h1>
-                        <p>Essa página apresenta algumas informações relevantes do sistema.</p>
+                        <h4>Este projeto tem como objetivo o agendamento de envio de mensagens em diferente sistemas/plataformas de comunicação.</h4>
+                        <p> 1. Na aba `Agendamento` será listado o status de cada agendamento.</p>
+                        <p> 2. No cabeçalho há 3 ícones responsáveis por adicionar, editar e excluir um agendamento.</p>
                     </div>
                 </div>
 
