15cff969678bc940dadb9b5a57431b75e407b814
Jonas Cesconetto <jonascesconetto@gmail.com>
2020-11-14 16:22:03 -0300

    Home page (empty)

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export class HomePage extends Component {
+
+    render() {
+        return (
+            
+            <div className="p-grid p-fluid dashboard">
+                
+                <div className="p-col-12">
+                    <div className="card">
+                        <h1>Home</h1>
+                        <p>Essa página apresenta algumas informações relevantes do sistema.</p>
+                    </div>
+                </div>
+
+            </div>
+        );
+    }
+}
